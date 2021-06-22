d2fd9a3b816fd04bc5b20a2717bd563b8daf93b9
Praveen Kumar <Praveen@micropyramid.com>
2021-06-22 22:12:56 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'registration';
+  registrationForm: FormGroup;
+  constructor(
+  ) {
+    this.registrationForm = new FormGroup({
+      firstName: new FormControl('',[Validators.required,Validators.minLength(3)]),
+      lastName: new FormControl('',[Validators.required,Validators.minLength(3)]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      mobile: new FormControl('', [Validators.required, Validators.maxLength(10),Validators.pattern("[789][0-9]{9}")]),
+      password: new FormControl('', [Validators.required,Validators.minLength(6)]),
+      password1: new FormControl('', [Validators.required,Validators.minLength(6)])
+    });
+  }
+
+  onSubmit() {
+    if(this.registrationForm.valid) {
+      console.log(this.formValue());
+    }
+  }
+  formValue() {
+    return this.registrationForm.value;
+  }
 }
